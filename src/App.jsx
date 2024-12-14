7a0484fa3b909423b6c2af3b521f30c3a35772d5
nuwan <nuwandharshana4@gmail.com>
2024-12-14 23:38:20 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,10 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-          <h1>Hello from Amplify</h1>
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>Hello from Amplify</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
